3d5ed419dea6da143d67d797577a467a8279cfc8
Douglas Queiroz <dqsl@cesar.org.br>
2015-03-17 18:59:57 -0300

    Aplicando o tema a turma

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150308225618) do
+ActiveRecord::Schema.define(version: 20150317201418) do
+
+  create_table "student_classes", force: true do |t|
+    t.string   "description"
+    t.integer  "turn"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
