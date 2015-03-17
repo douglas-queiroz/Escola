d6d0906b77f55bff102f5bf6d32dc8e04fdb1072
Douglas Queiroz <douglasq16@gmail.com>
2015-03-16 22:28:36 -0300

    Validações do professor

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,6 +2,20 @@ class Teacher < ActiveRecord::Base
 	has_one :user
 	accepts_nested_attributes_for :user, :allow_destroy => false
 
+	validates_presence_of :name, message: "Campo obrigatório"
+	validates_presence_of :birth, message: "Campo obrigatório"
+	validates_presence_of :sex, message: "Campo obrigatório"
+	validates_presence_of :cpf, message: "Campo obrigatório"
+	validates_presence_of :street, message: "Campo obrigatório"
+	validates_presence_of :cep, message: "Campo obrigatório"
+	validates_presence_of :comp, message: "Campo obrigatório"
+	validates_presence_of :neighborhood, message: "Campo obrigatório"
+	validates_presence_of :city, message: "Campo obrigatório"
+	validates_presence_of :state, message: "Campo obrigatório"
+	validates_presence_of :phone, message: "Campo obrigatório"
+
+	validates_cpf :cpf
+
 	Sex={
 		(MASCULINO = 0)=>"Masculino",
 		(FEMININO = 1)=>"Feminino"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
 	belongs_to :teacher
+
+	validates_presence_of :name, message: "Campo obrigatório"
+	validates_presence_of :password, message: "Campo obrigatório"
 end
