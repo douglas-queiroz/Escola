3db293d89c217ce3218f96283bd4848ae6cb7d0d
Douglas Queiroz <dqsl@cesar.org.br>
2015-03-18 14:01:28 -0300

    Ajustes do combobox

--- app/models/student_class.rb
+++ app/models/student_class.rb
@@ -1,3 +1,10 @@
 class StudentClass < ActiveRecord::Base
+
+	Turn = {
+		(MANHA = 0)=>"Manhã",
+		(TARDE = 1)=>"Tarde",
+		(NOITE = 2)=>"Noite",
+	}
+
 	validates_presence_of :description, message: "Campo obrigatório"
 end
