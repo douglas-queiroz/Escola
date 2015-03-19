4cc299b6c830c1b61162c8282cf026f9693e5f8d
Douglas Queiroz <dqsl@cesar.org.br>
2015-03-19 08:52:42 -0300

    estilo do cadastro de professores aplicado

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def formatar_data(data)
+		resp = ""
+		unless data.nil?
+		  resp = data.strftime("%d/%m/%Y")
+		end
+		return resp
+	end
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,2 +1,7 @@
 class Teacher < ActiveRecord::Base
+
+	Sex={
+		(MASCULINO = 0)=>"Masculino",
+		(FEMININO = 1)=>"Feminino"
+	}
 end
